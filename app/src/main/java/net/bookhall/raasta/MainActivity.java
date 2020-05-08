8cf272cd16b76c48e5cfad9d61c49d4b2bcd3180
kartikeysaran <kartikeysaran@users.noreply.github.com>
2020-05-08 14:28:01 +0530

    Completion of Project

--- app/src/main/java/net/bookhall/raasta/MainActivity.java
+++ app/src/main/java/net/bookhall/raasta/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String[] station = new String[]{"New Delhi","Rajiv chowk","Mandi House","Central Secretriat","Dilli Hat-INA","Mayur Vihar Phase-1","Karkardumba","Anand Vihar","Yamuna Bank","Vaishali","Lajpath nagar","Kalkaji Mandir","Hauz Khas","Botanical Garden",""};
+    private static final String[] station = new String[]{"New Delhi","Rajiv chowk","Mandi House","Central Secretriat","Dilli Hat-INA","Mayur Vihar Phase-1","Karkardumba","Anand Vihar","Yamuna Bank","Vaishali","Lajpath nagar","Kalkaji Mandir","Hauz Khas","Botanical Garden","Mandi House"};
     Button route;
 
 
@@ -26,33 +26,32 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,station);
         source.setAdapter(adapter);
 
-        final AutoCompleteTextView destination = findViewById(R.id.autoCompleteTextView3);
-        destination.setAdapter(adapter);
+
+
 
         route = findViewById(R.id.button);
         route.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                String msource = source.getText().toString();
-               String mdestination = destination.getText().toString();
+
 
 
 
                 if(TextUtils.isEmpty(msource)){
                source.setError("This Field Cannot be empty");
                 }
-                if(TextUtils.isEmpty(mdestination)) {
-                    destination.setError("This Field Cannot be empty");
-                }
+
+                else{
 
                 Intent i = new Intent(MainActivity.this,route.class);
 
                 i.putExtra("sour",msource);
-                i.putExtra("dest",mdestination);
+
 
                 startActivity(i);
 
-            }
+            }}
         });
 
     }
--- app/src/main/java/net/bookhall/raasta/route.java
+++ app/src/main/java/net/bookhall/raasta/route.java
@@ -2,65 +2,297 @@ package net.bookhall.raasta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ZoomControls;
 
 public class route extends AppCompatActivity {
     String msource,mdestination;
     TextView textView;
-    ImageView imageView;
-    int a=3,b=2,c=1,d=5,e=4,f=2,g=3,h=1,i=2,j=3,k=4,l=2,m=3,n=1,o=5,p=6,q=1,r=7,s=8;
+
+    ImageButton back;
+    Dialog myinfo,mymap;
+
+    private static final int NO_PARENT = -1;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
-        textView = (TextView)findViewById(R.id.textView4);
-        imageView = (ImageView)findViewById(R.id.imageView);
 
-        msource = getIntent().getExtras().getString("sour");
-        mdestination = getIntent().getExtras().getString("dest");
 
-       if(msource.equals("New Delhi")){
-           if(mdestination.equals("Rajiv chowk")){
-              textView.setText("Shortest route from "+msource+" to "+mdestination+" will take "+a+"minutes");
-           }
-           if(mdestination.equals("Central Secretriat")) {
+        setupUI();
 
-               if ((a + s) <= (a + b + r)) {
-                   textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a + s) + "minutes");
-               } else {
-                   textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a + b + r) + "minutes");
-               }
-           }
-               if(mdestination.equals("Dilli Hat-INA")){
-                   if((a+s+p)<(a+r+b+q+o)){
-                       textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a + s +p) + "minutes");
-                   }
-                   else{
-                       textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a +r+b+q+o) + "minutes");
-                   }
-               }
-               if(mdestination.equals("Hauj Khas")){
-                   if((a+b+r+q+o+n)<(a+b+r+q+l+m)){
-                       if((a+s+p+n)<(a+b+r+q+o+n)){
-                       textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a+s+p+n) + "minutes");
-                   }
-                       else{
-                           textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a+b+r+q+o+n) + "minutes");
-                       }
-               }
-               }
-                   else{
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-                       if((a+s+p+n)<(a+b+r+q+l+m)){
-                           textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a+s+p+n) + "minutes");
-                       }
-                       else{
-                           textView.setText("Shortest route from " + msource + " to " + mdestination + " will take " + (a+b+r+q+l+m) + "minutes");
-                       }
-                   }
+
+        int[][] adjacencyMatrix = {{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 4, 0, 0, 0},
+                {0, 0, 2, 1, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0},
+                {0, 1, 3, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 3, 0, 3, 0},
+                {0, 0, 0, 0, 0, 4, 0, 0, 0, 3, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        };
+
+        if(msource.equals("New Delhi")){
+            int a = 0;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Rajiv Chowk")){
+            int a = 1;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Central Secretriat")){
+            int a = 2;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Dilli Hat-INA")){
+            int a = 3;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Mayur Vihar Phase-1")){
+            int a = 9;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Karkardumba")){
+            int a = 11;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Anand Vihar")){
+            int a = 12;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Yamuna Bank")){
+            int a = 8;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Vaishali")){
+            int a = 13;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Lajpath nagar")){
+            int a = 6;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Kalkaji Mandir")){
+            int a = 5;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Botanical Garden")){
+            int a = 10;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Hauz Khas")){
+            int a = 4;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else if(msource.equals("Mandi House")){
+            int a = 7;
+            dijkstra(adjacencyMatrix, a);
+        }
+        else{
+            Toast.makeText(this, "Please select a valid station", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(route.this,MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+
        }
-}}
+
+    private void setupUI() {
+        textView = (TextView)findViewById(R.id.textView4);
+
+        back = findViewById(R.id.back);
+
+        mymap = new Dialog(this);
+        myinfo = new Dialog(this);
+        msource = getIntent().getExtras().getString("sour");
+
+    }
+
+
+
+    private void dijkstra(int[][] adjacencyMatrix,
+                          int startVertex)
+    {
+        int nVertices = adjacencyMatrix[0].length;
+        int[] shortestDistances = new int[nVertices];
+        boolean[] added = new boolean[nVertices];
+        for (int vertexIndex = 0; vertexIndex < nVertices;
+             vertexIndex++)
+        {
+            shortestDistances[vertexIndex] = Integer.MAX_VALUE;
+            added[vertexIndex] = false;
+        }
+        shortestDistances[startVertex] = 0;
+        int[] parents = new int[nVertices];
+        parents[startVertex] = NO_PARENT;
+        for (int i = 1; i < nVertices; i++)
+        {
+            int nearestVertex = -1;
+            int shortestDistance = Integer.MAX_VALUE;
+            for (int vertexIndex = 0;
+                 vertexIndex < nVertices;
+                 vertexIndex++)
+            {
+                if (!added[vertexIndex] &&
+                        shortestDistances[vertexIndex] <
+                                shortestDistance)
+                {
+                    nearestVertex = vertexIndex;
+                    shortestDistance = shortestDistances[vertexIndex];
+                }
+            }
+            added[nearestVertex] = true;
+            for (int vertexIndex = 0;
+                 vertexIndex < nVertices;
+                 vertexIndex++)
+            {
+                int edgeDistance = adjacencyMatrix[nearestVertex][vertexIndex];
+
+                if (edgeDistance > 0
+                        && ((shortestDistance + edgeDistance) <
+                        shortestDistances[vertexIndex]))
+                {
+                    parents[vertexIndex] = nearestVertex;
+                    shortestDistances[vertexIndex] = shortestDistance +
+                            edgeDistance;
+                }
+            }
+        }
+
+        printSolution(startVertex, shortestDistances, parents);
+    }
+    private void printSolution(int startVertex,
+                                      int[] distances,
+                                      int[] parents)
+    {
+        int nVertices = distances.length;
+
+
+        textView.setText("Src->Des\t\t\t Time\t\t\t Shortest Path");
+
+        for (int vertexIndex = 0;
+             vertexIndex < nVertices;
+             vertexIndex++)
+        {
+            if (vertexIndex != startVertex)
+            {
+                textView.setText(textView.getText()+"\n" + startVertex + " -> ");
+                textView.setText(textView.getText()+ String.valueOf(vertexIndex) + " \t\t\t\t\t\t\t\t\t ");
+                textView.setText(textView.getText()+String.valueOf(distances[vertexIndex]) + "\t\t\t\t\t");
+                printPath(vertexIndex, parents);
+            }
+        }
+    }
+
+
+    private void printPath(int currentVertex,
+                                  int[] parents)
+    {
+
+
+        if (currentVertex == NO_PARENT)
+        {
+            return;
+        }
+        printPath(parents[currentVertex], parents);
+        textView.setText(textView.getText()+String.valueOf(currentVertex) + " ");
+    }
+
+
+    public void showinfo(View v){
+        TextView close;
+        myinfo.setContentView(R.layout.info);
+        close = (TextView) myinfo.findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myinfo.dismiss();
+            }
+        });
+        myinfo.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myinfo.show();
+
+    }
+
+    public void showmap(View v){
+        TextView closemap;
+        final ZoomControls zoom;
+        final ImageView imgmap;
+        mymap.setContentView(R.layout.map);
+
+        imgmap = mymap.findViewById(R.id.imgmap);
+        zoom = mymap.findViewById(R.id.zoom);
+        zoom.hide();
+
+        imgmap.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                zoom.show();
+                return false;
+            }
+        });
+        zoom.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float x = imgmap.getScaleX();
+                float y = imgmap.getScaleY();
+
+                imgmap.setScaleX(x+1);
+                imgmap.setScaleY(y+1);
+                zoom.hide();
+
+            }
+        });
+        zoom.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final float x = imgmap.getScaleX();
+                final float y = imgmap.getScaleY();
+                    imgmap.setScaleX(x-1);
+                    imgmap.setScaleY(y-1);
+                    zoom.hide();
+
+            }
+        });
+
+
+        closemap = (TextView) mymap.findViewById(R.id.closemap);
+        closemap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mymap.dismiss();
+            }
+        });
+        mymap.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mymap.show();
+
+    }
+}
